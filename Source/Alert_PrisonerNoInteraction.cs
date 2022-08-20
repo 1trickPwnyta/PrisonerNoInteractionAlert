90cd4f494fcfc7323565c9360ae04fc91eeb1eb3
Sarah Pace <sarahpace37@gmail.com>
2022-08-19 18:44:41 -0700

    Initial commit

---
+++ Source/Alert_PrisonerNoInteraction.cs
@@ -0,0 +1,31 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using Verse;
+
+namespace PrisonerNoInteractionAlert
+{
+    public class Alert_PrisonerNoInteraction : Alert
+    {
+        private List<Pawn> GetCulprits()
+        {
+            List<Pawn> culprits = new List<Pawn>();
+            PawnsFinder.AllMaps_PrisonersOfColonySpawned.ForEach(pawn => {
+                if (pawn.GetExtraHostFaction() == null && pawn.guest.interactionMode == PrisonerInteractionModeDefOf.NoInteraction) culprits.Add(pawn);
+                // pawn.guest.interactionMode
+            });
+            return culprits;
+        }
+
+        public Alert_PrisonerNoInteraction()
+        {
+            this.defaultLabel = "PrisonerNoInteraction".Translate();
+            this.defaultExplanation = "PrisonerNoInteractionDesc".Translate();
+            this.defaultPriority = AlertPriority.High;
+        }
+
+        public override AlertReport GetReport()
+        {
+            return AlertReport.CulpritsAre(GetCulprits());
+        }
+    }
+}
---
+++ Source/PrisonerNoInteractionAlertMod.cs
@@ -0,0 +1,15 @@
+﻿using Verse;
+
+namespace PrisonerNoInteractionAlert
+{
+    public class PrisonerNoInteractionAlertMod : Mod
+    {
+        public const string PACKAGE_ID = "prisonernointeractionalert.1trickPwnyta";
+        public const string PACKAGE_NAME = "Prisoner No Interaction Alert";
+
+        public PrisonerNoInteractionAlertMod(ModContentPack content) : base(content)
+        {
+            Log.Message($"[{PACKAGE_NAME}] Loaded.");
+        }
+    }
+}
