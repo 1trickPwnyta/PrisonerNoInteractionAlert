1c85dc1871d1df5a127aee5b37f4e1383d8a0795
1trickPwnyta <sarahpace37@gmail.com>
2024-03-21 16:14:42 -0700

    Update to 1.5

---
+++ 1.5/Source/Debug.cs
@@ -0,0 +1,12 @@
+﻿namespace PrisonerNoInteractionAlert
+{
+    public static class Debug
+    {
+        public static void Log(string message)
+        {
+#if DEBUG
+            Verse.Log.Message($"[{PrisonerNoInteractionAlertMod.PACKAGE_NAME}] {message}");
+#endif
+        }
+    }
+}
